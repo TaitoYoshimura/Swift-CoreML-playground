cac5ff84ac63c72332434eab95e0212634b4d311
TaitoYoshimura <pz6f9vej@s.okayama-u.ac.jp>
2025-10-29 16:43:14 +0900

    initial setup

---
+++ coreML-playground/coreML_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreML_playgroundApp.swift
+//  coreML-playground
+//
+//  Created by yoshimura on 2025/10/29.
+//
+
+import SwiftUI
+
+@main
+struct coreML_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
